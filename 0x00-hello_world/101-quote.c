eebeb3d734de1aed2a1ff58d4a794b6964e10bba
Oreoluwa <oreoluwaoyetubo@gmail.com>
2022-09-08 16:21:52 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <unistd.h>
 
 /**
- * main -prints a standard error
+ * main -prints a text
  *
- * return: Always 1 (success)
+ * Return: Always 1 (Success)
  */
 
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
