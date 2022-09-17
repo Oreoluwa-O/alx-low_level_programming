afb5fc51e0886b6ee6f5e9d71820f4b9d9a829ed
Oreoluwa <oreoluwaoyetubo@gmail.com>
2022-09-16 17:02:23 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,10 +1,10 @@
 #include"main.h"
 
 /**
- *print_line - print a straight line
+ * print_line - print a straight line
  *
- *@n: is the number of times the _ character
- *should be printed
+ * @n: is the number of times the _ character
+ *     should be printed
  */
 
 void print_line(int n)
@@ -16,7 +16,7 @@ void print_line(int n)
 	else
 	{
 		for (lnChr = 1; lnChr <= n; ++lnChr)
-			_putchar('_');
-		_putchar('/n');
+			putchar('');
+		_putchar('\n');
 	}
 }
